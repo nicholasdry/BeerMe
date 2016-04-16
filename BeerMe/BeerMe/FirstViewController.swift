3968bdb8036043f50061ae78b0cc0d9e42bf3a23
Nicholas Dry <nicholasdry@me.com>
2016-04-16 13:03:21 -0400

    Fixed ViewDidLoad

--- BeerMe/BeerMe/FirstViewController.swift
+++ BeerMe/BeerMe/FirstViewController.swift
@@ -10,47 +10,41 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    var temp = BeerItem(alc: 12.3, ibu: 3, beer: "Name", stat: "NJ")
-    
-    /** IBU Arrays **/
-    var to10: [Int] = []
-    var to20 = [BeerItem]()
-    var to30 = [BeerItem]()
-    var to40 = [BeerItem]()
-    var to50 = [BeerItem]()
-    var to60 = [BeerItem]()
-    var to70 = [BeerItem]()
-    var to80 = [BeerItem]()
-    var to90 = [BeerItem]()
-    var to100 = [BeerItem]()
-    
-    
-    /** ABV Arrays **/
-    var per1 = [BeerItem]()
-    var per2 = [BeerItem]()
-    var per3 = [BeerItem]()
-    var per4 = [BeerItem]()
-    var per5 = [BeerItem]()
-    var per6 = [BeerItem]()
-    var per7 = [BeerItem]()
-    var per8 = [BeerItem]()
-    var per9 = [BeerItem]()
-    var per10 = [BeerItem]()
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var to10 = [BeerItem]()
+        var to20 = [BeerItem]()
+        var to30 = [BeerItem]()
+        var to40 = [BeerItem]()
+        var to50 = [BeerItem]()
+        var to60 = [BeerItem]()
+        var to70 = [BeerItem]()
+        var to80 = [BeerItem]()
+        var to90 = [BeerItem]()
+        var to100 = [BeerItem]()
+        
+        var per1 = [BeerItem]()
+        var per2 = [BeerItem]()
+        var per3 = [BeerItem]()
+        var per4 = [BeerItem]()
+        var per5 = [BeerItem]()
+        var per6 = [BeerItem]()
+        var per7 = [BeerItem]()
+        var per8 = [BeerItem]()
+        var per9 = [BeerItem]()
+        var per10 = [BeerItem]()
+        
+        var temp = BeerItem(alc: 1, ibu: 1, beer: "1", stat: "werfg")
+        to10.append(temp)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func addBeer(beer: BeerItem) {
-        to10.append(beer)
     }
 
     
