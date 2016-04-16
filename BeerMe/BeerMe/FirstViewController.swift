03ba73b0f80d6298daf20f7baffb73b0489f7a9a
Nicholas Dry <nicholasdry@me.com>
2016-04-16 12:12:17 -0400

    Uploaded

--- BeerMe/BeerMe/FirstViewController.swift
+++ BeerMe/BeerMe/FirstViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    
+    struct BeerItem {
+        var alcoholContent:Double
+        var ibuCount:Int
+        var name:String
+        var state:String
+    }
+    
+    
+    /** IBU Arrays **/
+    var to10 = [BeerItem]()
+    var to20 = [BeerItem]()
+    var to30 = [BeerItem]()
+    var to40 = [BeerItem]()
+    var to50 = [BeerItem]()
+    var to60 = [BeerItem]()
+    var to70 = [BeerItem]()
+    var to80 = [BeerItem]()
+    var to90 = [BeerItem]()
+    var to100 = [BeerItem]()
+    
+    
+    /** ABV Arrays **/
+    var per1 = [BeerItem]()
+    var per2 = [BeerItem]()
+    var per3 = [BeerItem]()
+    var per4 = [BeerItem]()
+    var per5 = [BeerItem]()
+    var per6 = [BeerItem]()
+    var per7 = [BeerItem]()
+    var per8 = [BeerItem]()
+    var per9 = [BeerItem]()
+    var per10 = [BeerItem]()
+    
     @IBOutlet var textOnScreen: UILabel!
     
     var count = 0
