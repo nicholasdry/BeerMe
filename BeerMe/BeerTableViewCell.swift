71701bd00c5e74a74d9fa062b64a19782e4661ed
Hemil Patel <hemilmpatel@yahoo.com>
2016-04-16 16:13:02 -0400

    changes

---
+++ BeerMe/BeerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeerTableViewCell.swift
+//  BeerMe
+//
+//  Created by Hemil Patel on 4/16/16.
+//  Copyright © 2016 Nicholas Dry. All rights reserved.
+//
+
+import UIKit
+
+class BeerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
