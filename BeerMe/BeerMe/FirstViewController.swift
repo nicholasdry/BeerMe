3e94a7eab340b538dc91211a89f9d2f76148adbe
Hemil Patel <hemilmpatel@yahoo.com>
2016-04-16 13:29:17 -0400

    finished to10

--- BeerMe/BeerMe/FirstViewController.swift
+++ BeerMe/BeerMe/FirstViewController.swift
@@ -25,7 +25,18 @@ class FirstViewController: UIViewController {
         var to90 = [BeerItem]()
         var to100 = [BeerItem]()
         
-        to10.append(BeerItem(alc: <#T##Double#>, ibu: <#T##Int#>, beer: <#T##String#>, stat: <#T##String#>))
+        to10.append(BeerItem(alc: 5.5, ibu: 25, beer: "Funnel Cake NITRO", stat: "NJ", brew: "Forgotten BoardWalk"))
+        to10.append(BeerItem(alc: 5.5, ibu: 25, beer: "Funnel Cake", stat: "NJ", brew: "Forgotten BoardWalk"))
+        to10.append(BeerItem(alc: 6.9, ibu: 65, beer: "1916 Shore Shiver", stat: "NJ", brew: "Forgotten BoardWalk"))
+        to10.append(BeerItem(alc: 8.5, ibu: 100, beer: "Pocket Trick", stat: "NJ", brew: "Forgotten BoardWalk"))
+        to10.append(BeerItem(alc: 5.0, ibu: 18, beer: "Lady #6 (Cucumber & Ginger)", stat: "NJ", brew: "Cape May Brewing Company"))
+        to10.append(BeerItem(alc: 7.3, ibu: 25, beer: "Mop Water", stat: "NJ", brew: "Cape May Brewing Company"))
+        to10.append(BeerItem(alc: 8.6, ibu: 25, beer: "Devil's Reach", stat: "NJ", brew: "Cape May Brewing Company"))
+        to10.append(BeerItem(alc: 5.5, ibu: 60, beer: "Poverty Beach", stat: "NJ", brew: "Cape May Brewing Company"))
+        to10.append(BeerItem(alc: 5.0, ibu: 10, beer: "I Know What You Did Last Shandy", stat: "NJ", brew: "Cape May Brewing Company"))
+        to10.append(BeerItem(alc: 9.2, ibu: 80, beer: "Concrete Ship", stat: "NJ", brew: "Cape May Brewing Company"))
+        
+        to20.append(BeerItem(alc: <#T##Double#>, ibu: <#T##Int#>, beer: <#T##String#>, stat: <#T##String#>, brew: <#T##String#>))
         
         
         var per1 = [BeerItem]()
@@ -38,9 +49,6 @@ class FirstViewController: UIViewController {
         var per8 = [BeerItem]()
         var per9 = [BeerItem]()
         var per10 = [BeerItem]()
-        
-        var temp = BeerItem(alc: 1, ibu: 1, beer: "1", stat: "werfg")
-        to10.append(temp)
         
         
     }
